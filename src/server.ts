d082566547d9e6c9b2e0b1e33967f9e83b303af0
Daniel Hessel <hesseldaniel21@gmail.com>
2021-11-26 15:18:41 -0300

    start server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
 import 'dotenv/config';
+
+import express from 'express';
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(process.env.PORT || 3333, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT || 3333}!`);
+});
