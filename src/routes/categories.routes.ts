4cbb95c1374b5d350e15228ddce185a33aa565ee
Daniel Hessel <hesseldaniel21@gmail.com>
2021-12-21 18:15:40 -0300

    feat: creating file uploads

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/CreateCategory';
 import { listCategoriesController } from '../modules/cars/useCases/ListCategories';
 
 const categoriesRouter = Router();
+
+const upload = multer({ dest: './tmp' });
 
 categoriesRouter.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +14,13 @@ categoriesRouter.post('/', (request, response) => {
 
 categoriesRouter.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRouter.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+  console.log(file);
+
+  response.send();
 });
 
 export { categoriesRouter };
